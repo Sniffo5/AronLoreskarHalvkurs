d81a2f1ac3fbc7436caac3b156abe5ee1490ce47
Aron Loreskär <arolor0730@edu.halmstad.se>
2024-11-15 14:13:25 +0100

    lektion 4

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-const express = require("express");
+const express = require('express');
+const { execArgv } = require('process');
+const bcrypt = require("bcryptjs");
+const session = require('express-session');
 const app = express();
-app.listen(3000, () => console.log("http://localhost:3000/"));
+app.use(express.urlencoded({extended:true}));
 const fs = require("fs");
-
-app.use(express.static("public"));
-
 const {render, div} = require("./utils.js");
 
+app.use(express.static("public"));
+app.listen(3000, () => console.log("http://localhost:3000/"));
+
 app.get("/", home);
-
-function home(req, res){
-    res.send(render(div("t", "c")));
-}
-
 app.get("/login", login);
 app.get("/registrera", register);
 app.get("/tjanster", tjanster);
+app.get("/skapatjanst", showCreateTjanst);
 app.get("/skapatjanst", createTjanst);
+
+
+
+
+
+function home(req, res){
+    res.send(render(fs.readFileSync("templates/createTjanst.html").toString()));
+}
+
+
 
 
 function login(){};
 function tjanster(){};
+
 function register(){
 
     if (x){
+
+
 
     }
     else {
@@ -33,7 +45,7 @@ function register(){
 
 
 };
-function createTjanst(){
+function showCreateTjanst(){
 
     let x = "1";
     let y = "2"
@@ -45,4 +57,14 @@ function createTjanst(){
         res.redirect("/login");
     }
 };
+
+function createTjanst(req, res){
+    let data = req.body;
+
+    let services = JSON.parse(fs.readFileSync("services.json").toString());
+
+    services.push(data);
+
+    fs.writeFileSync("services.json", JSON.stringify(services, null, 3));
+}
 
